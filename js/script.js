8e30ac1a07424f744088bbe0a0c956a11c6d4185
Brii <nathaliab.locatelli@gmail.com>
2026-09-21 15:26:43 -0300

    Display Pokemon data from PokeAPI

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 fetch("https://pokeapi.co/api/v2/pokemon/1")
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => {
+        const container = document.getElementById("pokemon-container");
+        container.textContent = data.name;
+        const number = document.createElement("p");
+        number.textContent = `#${data.id}`;
+        container.appendChild(number);
+        const image = document.createElement("img");
+        image.src = data.sprites.front_default;
+        container.appendChild(image);
+    });
