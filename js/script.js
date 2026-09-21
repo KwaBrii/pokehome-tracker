d667561b97341e68268a1f9a2f035d23de13103a
Brii <nathaliab.locatelli@gmail.com>
2026-09-21 15:36:40 -0300

    Create Pokemon loop

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
-fetch("https://pokeapi.co/api/v2/pokemon/1")
-    .then(response => response.json())
-    .then(data => {
-        const container = document.getElementById("pokemon-container");
-        container.textContent = data.name;
-        const number = document.createElement("p");
-        number.textContent = `#${data.id}`;
-        container.appendChild(number);
-        const image = document.createElement("img");
-        image.src = data.sprites.front_default;
-        container.appendChild(image);
-    });
+function getPokemon(id) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        .then(response => response.json())
+        .then(data => {
+            const container = document.getElementById("pokemon-container");
+
+            container.textContent = data.name;
+
+            const number = document.createElement("p");
+            number.textContent = `#${data.id}`;
+            container.appendChild(number);
+
+            const image = document.createElement("img");
+            image.src = data.sprites.front_default;
+            container.appendChild(image);
+        });
+}
+for (let id = 1; id <= 10; id++) {
+    getPokemon(id);
+}
